839b42c812c78621074c802954ffc8dae690b463
sharifjonovyusufjon <Yusufjon6727@gmail.com>
2025-04-02 15:25:37 +0900

    feat: develop first state travel list && travel details

--- travel/urls.py
+++ travel/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main')
+    path('', views.main, name='main'),
+    path('travel/', views.travels, name='travels'),
+    path('travel/details/<int:id>', views.details, name='details'),
 ]
--- travel/views.py
+++ travel/views.py
@@ -11,3 +11,19 @@ def main(request):
         'travels': travels,
     }
     return HttpResponse(template.render(context, request))
+
+def travels(request):
+    travels = Travel.objects.all().values()
+    template = loader.get_template('travelsList.html')
+    context = {
+        'travels': travels,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    travel = Travel.objects.get(id=id)
+    template = loader.get_template('travelDetails.html')
+    context = {
+        'travel': travel
+    }
+    return HttpResponse(template.render(context, request))
